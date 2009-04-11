8a2dac568a68151ad869bb92eb1fa5d91522db05
César D. Rodas <saddor@gmail.com>
2009-04-11 19:55:25 -0400

    HAving little bug on patch_delta, the missing thing to have the getObject totally functional

--- git.php
+++ git.php
@@ -1,12 +1,11 @@
 <?php
-define("OBJ_COMMIT",    16);
-define("OBJ_TREE",      32);
-define("OBJ_BLOB",      48);
-define("OBJ_TAG",       64);
-define("OBJ_OFS_DELTA", 96);
-define("OBJ_REF_DELTA", 112);
+define("OBJ_COMMIT",    1);
+define("OBJ_TREE",      2);
+define("OBJ_BLOB",      3);
+define("OBJ_TAG",       4);
+define("OBJ_OFS_DELTA", 6);
+define("OBJ_REF_DELTA", 7);
 define("GIT_INVALID_INDEX", 0x02);
-//define("PACK_IDX_SIGNATURE",0xff744f63);
 define("PACK_IDX_SIGNATURE","\377tOc");
 
 abstract class GitBase {
@@ -73,8 +72,12 @@ abstract class GitBase {
             return gzinflate(substr($content,2));
         } else {
             $obj = $this->getPackedObject($id);
-            die("missing file");
+            if ($obj !== false) {
+                return $obj[1];
+            }
         }
+        $this->Exception("object not found $id");
+        return false;
     }
 
     final protected function _sha1_to_hex($sha1) {
@@ -87,28 +90,35 @@ abstract class GitBase {
         return $str;
     }
 
+    final public function getNumber($bytes) {
+        $c = unpack("N",$bytes);
+        return $c[1];
+    }
+
     final private function getIndexInfo($path) {
         if (isset($this->index[$path]))
             return $this->index[$path];
         $content    = $this->getFileContents($path,false,true);
         $version    = 1;
+        $hoffset    = 0;
         if (substr($content,0,4) == PACK_IDX_SIGNATURE) {
             $version = $this->getNumber(substr($content,4,4));
             if ($version != 2) {
                 $this->Exception("The pack-id's version is $version, PHPGit only supports versio 1 or 2, please update this package, or downgrade your git repo");
             }
+            $hoffset = 8;
         }
+        $indexes = unpack("N*",substr($content,$hoffset,256*4));
+        $nr = 0;
+        for($i=0; $i < 256; $i++) {
+            if (!isset($indexes[$i+1])) continue;
+            $n =  $indexes[$i+1];
+            if ($n < $nr) 
+                $this->Exception("corrupt index file ($n,$nr)\n");
+            $nr = $n;
+        }   
+        $_offset = $hoffset + 256 * 4;
         if ($version == 1) {
-            $_offset = 256 * 4;
-            $indexes = unpack("N*",substr($content,0,256*4));
-            $nr = 0;
-            for($i=0; $i < 256; $i++) {
-                if (!isset($indexes[$i+1])) continue;
-                $n =  $indexes[$i+1];
-                if ($n < $nr) 
-                    $this->Exception("corrupt index file ($n,$nr)\n");
-                $nr = $n;
-            }   
             $offset = $_offset;
             for($i=0; $i < $nr; $i++) {
                 $field = substr($content,$offset,24);
@@ -118,28 +128,199 @@ abstract class GitBase {
             }
             $this->index[$path] = $tmp;
         } else if ($version == 2) {
+            $offset = $_offset;
+            $keys = $data = array();
+            for($i=0; $i < $nr;  $i++) {
+                $keys[] = $this->_sha1_to_hex(substr($content,$offset,20));
+                $offset += 20;
+            } 
+            for ($i=0; $i < $nr; $i++) {
+                $offset += 4;
+            }
+            for ($i=0; $i < $nr; $i++) {
+                $data[] = $this->getNumber(substr($content,$offset,4));
+                $offset += 4;
+            }
+            $this->index[$path] = array_combine($keys,$data);
         }
         return $this->index[$path];
     }
 
-    final private function getPackedObject($id) {
+    final private function getPackedObject($id,&$type=null) {
         /* load packages */
         foreach(glob($this->dir."/objects/pack/*.idx") as $findex) {
             $index = $this->getIndexInfo($findex);
             if (isset($index[$id]))  {
+                $start  = $index[$id];
+                /* open pack file */
+                $pack_file = substr($findex,0,strlen($findex)-3)."pack";
+                $fp     = fopen($pack_file, "rb");
 
+                $object =  $this->unpack_object($fp,$start);
+
+                fclose($fp);
+
+                return $object;
             }
         }
         return false;
     }
 
+    final private function unpack_object($fp,$start) {
+        /* offset till the start of the object */
+        fseek($fp,$start, SEEK_SET);
+        /* read first byte, and get info */
+        $header = ord(fread($fp,1));
+        $type   = ($header >> 4) & 7;
+        $hasnext= ($header & 128) >> 7; 
+        $size   = $header & 0xf;
+        $offset = 4;
+        /* read size bytes */
+        while ($hasnext) {
+            $byte  = ord(fread($fp,1)); 
+            $size |= ($byte & 0x7f) << $offset; 
+            $hasnext= ($byte & 128) >> 7; 
+            $offset+=7;
+        }
+
+        switch ($type) {
+            case OBJ_COMMIT:
+            case OBJ_TREE:
+            case OBJ_BLOB:
+            case OBJ_TAG:
+                $obj = $this->unpack_compressed($fp,$size);
+                return array($type,$obj);
+                break;
+            case OBJ_OFS_DELTA:
+            case OBJ_REF_DELTA:
+                $obj = $this->unpack_delta($fp,$start,$type,$size);
+                return array($type,$obj);
+                break;
+            default:
+                $this->Exception("Unkown object type $type");
+        }
+    }
+
+    final private function unpack_compressed($fp, $size) {
+        fseek($fp,2,SEEK_CUR);
+        $out ="";
+        do {
+            $cstr = fread($fp,4096);
+            $uncompressed = gzinflate($cstr);
+            if ($uncompressed === false) {
+                $this->Exception("fatal error while uncompressing at position ".ftell($fp));
+            }
+            $out .= $uncompressed; 
+        } while (strlen($out) < $size);
+        if ($size != strlen($out)) 
+            $this->Exception("Weird error, the packed object size mismatch with the readed size");
+        return $out;
+    }
+
+    final private function unpack_delta($fp,$obj_start,$type,$size) {
+        $delta_offset = ftell($fp);
+        $sha1 = fread($fp,20);
+        if ($type == OBJ_OFS_DELTA) {
+            $i = 0;
+            $c = ord($sha1[$i]);
+            $offset = $c & 0x7f;
+            while (($c & 0x80) != 0)  {
+                $c = ord($sha1[ ++$i ]);
+                $offset += 1;
+                $offset <<= 7;
+                $offset |= $c & 0x7f;
+            }
+            $offset = $obj_start - $offset;
+            $i++;
+        } else {
+            die("not implemented");
+        }
+        /* unpack object */
+        list($type,$base) = $this->unpack_object($fp,$offset);
+        echo "$type = ".strlen($base)."\n";
+        /* get compressed delta */
+        fseek($fp,$delta_offset+$i,SEEK_SET);
+        $delta = $this->unpack_compressed($fp,$size); 
+
+        /* patch the base with the delta */
+        $obj = $this->patch_object($base, $delta);
+
+        return $obj;
+    }
+
+    final protected function patch_delta_header_size($delta,$pos) {
+        $size = $shift = 0;
+        do {
+            $byte = ord($delta[$pos]);
+            if ($byte == null) {
+                $this->Exception("Unexpected delta's end.");
+            }
+            $pos++;
+            $size |= ($byte & 0x7f) << $shift;
+            $shift += 7;
+        } while (($byte & 0x80) != 0);
+        return array($size, $pos);
+    }
+
+    final protected function patch_object($base,$delta) {
+        list($src_size,$pos) = $this->patch_delta_header_size($delta,0);
+        if ($src_size != strlen($base)) {
+            $this->Exception("Invalid delta data size ".$src_size." ".strlen($base));
+        }
+        list($dst_size,$pos) = $this->patch_delta_header_size($delta,$pos);
+        $dest = "";
+        $delta_size = strlen($delta);
+        while ($pos <= $delta_size) {
+            $byte = ord($delta[$pos++]);
+            if ( ($byte&0x80) != 0 ) {
+                $pos--;
+                $cp_off = $cp_size = 0;
+                /* fetch start position */
+                $flags = array(0x01,0x02,0x04,0x08);
+                for($i=0; $i < 4; $i++) {
+                    if ( ($byte & $flags[$i]) != 0) 
+                        $cp_off |= ord($delta[$pos++]) << ($i * 8);
+                }
+                /* fetch length  */
+                $flags = array(0x10,0x20,0x40);
+                for($i=0; $i < 3; $i++) {
+                    if ( ($byte & $flags[$i]) != 0) {
+                        $cp_size |= ord($delta[$pos++]) << ($i * 8);
+                    }
+                }
+                /* default length */
+                if ($cp_size === 0) 
+                    $cp_size = 0x10000;
+                $part = substr($base,$cp_off,$cp_size);
+                if (strlen($part) != $cp_size) {
+                    var_dump(strlen($part),$cp_off,$cp_size);
+                    die();
+                }
+                $pos++;
+            } else if ($byte != 0) {
+                $part = substr($delta,$pos,$byte);
+                if (strlen($part) != $byte) {
+                    var_dump(strlen($part),$cp_off,$cp_size);
+                    die("1");
+                }
+                $pos += $byte;
+            } else {
+                $this->Exception("Invalid delta data");
+            }
+            $dest .= $part;
+        }
+        if (strlen($dest) != $dst_size) die(strlen($dest)." $dst_size error");
+        return $dest;
+    }
 
     final protected function simpleParsing($text,$limit=-1, $sep=' ', $findex=true) {
         $return = array();
         $i = 0;
         foreach(explode("\n",$text) as $line) {
             if ($limit != -1 && $limit < ++$i ) break; 
-            list($first,$second) = explode($sep,$line,2);
+            $info = explode($sep,$line,2);
+            if (count($info) != 2) break;
+            list($first,$second) = $info; 
             $return[ $findex ? $first : $second ] = $findex ? $second : $first;
         }
         return $return;
@@ -162,19 +343,30 @@ class Git extends GitBase {
         }
         $object_id = $this->branch[$branch];
 
+        $history = array();
         do { 
-            $commit    = $this->simpleParsing($this->getObject($object_id),4);
-            $object[]  = $commit;
-            $object_id = $commit['parent'];
+            $object_text = $this->getObject($object_id);
+            $commit    = $this->simpleParsing($object_text,4);
+            $commit['comment'] = trim(strstr($object_text,"\n\n")); 
+            $history[$object_id]  = $commit;
+            $object_id = isset($commit['parent']) ? $commit['parent'] : false;
         } while (strlen($object_id) > 0);
-        var_dump($history);
+        return $history;
     }    
+
+    function getCommit($id) {
+        $commit = $this->getObject($id);
+        var_dump($commit);
+    }
 }
 
 $repo = new Git("/home/crodas/projects/playground/phpserver/phplibtextcat/.git");
-$repo = new Git("/home/crodas/projects/bigfs/.git");
+//$repo = new Git("/home/crodas/projects/bigfs/.git");
 var_dump($repo->getBranches());
-var_dump($repo->getHistory('master'));
+//$history = $repo->getHistory('master');
+//var_dump($history);
+$level=0;
+$repo->getcommit('b22d9c85cd28af4a4c8059614521cb42d94ade49');
 
 
 ?>
